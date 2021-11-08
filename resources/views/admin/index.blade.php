1efb247e9851fab40c94d22f918233464284c705
karoline bustinza <karoline.bustinza@tecsup.edu.pe>
2021-11-08 00:58:08 -0500

    first commit

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,117 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+
+@section('content_header')
+    <h1>Administrador</h1>
+@stop
+
+@section('content')
+
+<div class="card">
+    <div class="card-header">
+        <h1 class="card-title">Hola administrador</h1>
+    </div>
+
+
+    <div class="card-body">
+        <p>lorem ipsum dolor sit amet conetur</p>
+    </div>
+</div>
+<div class="card">
+    <div class="card-header">
+        <h1 class="card-title">Hola administrador</h1>
+    </div>
+
+
+    <div class="card-body">
+        <p>lorem ipsum dolor sit amet conetur</p>
+    </div>
+</div>
+
+<div class="card">
+    <div class="card-header">
+        <h1 class="card-title">Hola administrador</h1>
+    </div>
+
+
+    <div class="card-body">
+        <p>lorem ipsum dolor sit amet conetur</p>
+    </div>
+</div>
+
+<div class="card">
+    <div class="card-header">
+        <h1 class="card-title">Hola administrador</h1>
+    </div>
+
+
+    <div class="card-body">
+        <p>lorem ipsum dolor sit amet conetur</p>
+    </div>
+</div>
+
+<div class="card">
+    <div class="card-header">
+        <h1 class="card-title">Hola administrador</h1>
+    </div>
+
+
+    <div class="card-body">
+        <p>lorem ipsum dolor sit amet conetur</p>
+    </div>
+</div>
+
+<div class="card">
+    <div class="card-header">
+        <h1 class="card-title">Hola administrador</h1>
+    </div>
+
+
+    <div class="card-body">
+        <p>lorem ipsum dolor sit amet conetur</p>
+    </div>
+</div>
+
+<div class="card">
+    <div class="card-header">
+        <h1 class="card-title">Hola administrador</h1>
+    </div>
+
+
+    <div class="card-body">
+        <p>lorem ipsum dolor sit amet conetur</p>
+    </div>
+</div>
+
+<div class="card">
+    <div class="card-header">
+        <h1 class="card-title">Hola administrador</h1>
+    </div>
+
+
+    <div class="card-body">
+        <p>lorem ipsum dolor sit amet conetur</p>
+    </div>
+</div>
+
+
+
+
+  
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script>
+    Swal.fire(
+        'Good job!',
+        'You clicked the button!',
+        'success'
+      )
+     </script>
+@stop
